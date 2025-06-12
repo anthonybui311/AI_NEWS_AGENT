8f423066f00cd3948275e3bdbdbedbb86c9bbe0f
Viet Bui <anthonybui311205@gmail.com>
2025-06-12 08:33:06 +0700

    Make change to uncomment some configurations

--- news_agent/configuration/config.py
+++ news_agent/configuration/config.py
@@ -10,19 +10,19 @@ EMAIL_ADDRESS=os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD=os.getenv("EMAIL_PASSWORD")
 RECIPIENT_EMAIL=os.getenv("RECIPIENT_EMAIL")
 API_KEY=os.getenv("API_KEY")
-MAX_ARTICLES = 4
+MAX_ARTICLES = 2
 
 NEWS_SOURCES = {
-    # "Thế giới": "https://vnexpress.net/the-gioi",
-    # "Thời sự": "https://vnexpress.net/thoi-su",
-    # "Kinh doanh": "https://vnexpress.net/kinh-doanh",
-    # # "Góc nhìn": "https://vnexpress.net/goc-nhin",
-    # # "Giải trí": "https://vnexpress.net/giai-tri",
-    # "Thể thao": "https://vnexpress.net/the-thao",
-    # # "Pháp luật": "https://vnexpress.net/phap-luat",
-    # "Giáo dục": "https://vnexpress.net/giao-duc",
-    # # "Sức khỏe": "https://vnexpress.net/suc-khoe",
-    # # "Đời sống": "https://vnexpress.net/doi-song",
-    # # "Du lịch": "https://vnexpress.net/du-lich",
+    "Thế giới": "https://vnexpress.net/the-gioi",
+    "Thời sự": "https://vnexpress.net/thoi-su",
+    "Kinh doanh": "https://vnexpress.net/kinh-doanh",
+    "Góc nhìn": "https://vnexpress.net/goc-nhin",
+    "Giải trí": "https://vnexpress.net/giai-tri",
+    "Thể thao": "https://vnexpress.net/the-thao",
+    "Pháp luật": "https://vnexpress.net/phap-luat",
+    "Giáo dục": "https://vnexpress.net/giao-duc",
+    "Sức khỏe": "https://vnexpress.net/suc-khoe",
+    "Đời sống": "https://vnexpress.net/doi-song",
+    "Du lịch": "https://vnexpress.net/du-lich",
     "Khoa học công nghệ": "https://vnexpress.net/khoa-hoc-cong-nghe",
     }
